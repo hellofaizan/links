b429639bd6410f2fc4028e3a6585b3b90d2f5b3c
Mohammad Faizan <mofaizanfoundations@gmail.com>
2023-02-18 01:30:58 +0530

    Initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { Inter } from '@next/font/google'
+import { useState, useEffect } from 'react'
+
+const Home = () => {  
+  const [data, setData] = useState(null);
+  const [isLoading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    fetch('https://api.npoint.io/4a8d62649d30ab3f091e', {
+      method: 'GET',
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+        setLoading(false);
+        console.log(data);
+      });
+  }, []);
+
+  if (isLoading) return <p className='w-screen h-screen flex justify-center items-center'>Loading...</p>;
+  if (!data) return <p className='w-screen h-screen flex justify-center items-center'>No profile data</p>;
+  
+  return (
+    <>
+    <div>
+      <p className="text-3xl font-bold underline">
+        Hello World
+      </p>
+    </div>
+    </>
+  )
+}
+
+export default Home
